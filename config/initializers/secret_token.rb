d7f8a44428d5df80965967be5bc175b3bb2db806
Shaun <sgtoken@gmail.com>
2013-07-09 10:18:53 +0300

    First commit pre-heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FSApp::Application.config.secret_key_base = '80c8a6b33319696c90768a6a6531f2b46397f6dd5edf9cda36d5a6285326f0c58c17b61d0271893d0853a80218bb3cbd9274b79ebe76134b874b460c9c3aa37c'
